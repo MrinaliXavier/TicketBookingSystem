c68ce1967c9441964a1ad7da9271a47f564a8671
MrinaliXavier <xmrinali@gmail.com>
2024-12-11 10:07:56 +0530

    added pool capacity to config form

--- src/app/pages/configuration-form/configuration-form.component.ts
+++ src/app/pages/configuration-form/configuration-form.component.ts
@@ -8,6 +8,7 @@ import {FormsModule, NgModel} from "@angular/forms";
 })
 export class ConfigurationFormComponent {
   totalTickets: number = 100;
+  poolCapacity : number = 50;
   ticketReleaseRate: number = 5;
   customerRetrievalRate: number = 3;
   vendorCount: number = 3;
@@ -17,6 +18,7 @@ export class ConfigurationFormComponent {
   onSave() {
     const config = {
       totalTickets: this.totalTickets,
+      poolCapacity: this.poolCapacity,
       ticketReleaseRate: this.ticketReleaseRate,
       customerRetrievalRate: this.customerRetrievalRate,
       vendorCount: this.vendorCount
